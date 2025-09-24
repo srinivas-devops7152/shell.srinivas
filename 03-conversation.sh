350bd921d554349b63e8dc5167d8833e62749b85
srinivas-devops7152 <idurgasrinivas@gmail.com>
2025-09-24 17:12:28 +0530

    echo

--- 03-conversation.sh
+++ 03-conversation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+
 echo "Ramesh:: Hii suresh,how are you?"
 echo "suresh:: hellow ramesh,I am fine.How are ramesh"
---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# N space before or after equal
+PERSON1=John
+PERSON2=Raheem
+
+echo "$PERSON1:: Hii $PERSON2,how are you?"
+echo "$PERSON2:: hellow$PERSON1,I am fine.How are $PERSON1"
