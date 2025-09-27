a40c2e3793b97124d6159d8a6376d6583fa5cb53
srinivas-devops7152 <idurgasrinivas@gmail.com>
2025-09-27 13:23:25 +0530

    robo shop tada

--- 04-variables.sh
+++ 04-variables.sh
@@ -2,7 +2,7 @@
 
 # N space before or after equal
 PERSON1=Ganesh
-PERSON2=Shyam
+PERSON2=Shyam 
 
 echo "$PERSON1:: Hii $PERSON2,how are you?"
 echo "$PERSON2:: hellow$PERSON1,I am fine.How are $PERSON1"
