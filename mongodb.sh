af05c0149a63d75721e6e0dec892feed4f7e994c
srinivas-devops7152 <idurgasrinivas@gmail.com>
2025-09-28 16:09:23 +0530

    robo shop tada

--- mongodb.sh
+++ mongodb.sh
@@ -27,7 +27,7 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongodb.sh /etc/yum.mongodb.sh
 VALIDATE $? "Adding Mongo repo"
 
 dnf install mongodb-org -y &>>$LOG_FILE
