50a49a4ecdbb8c66969c475c2248e1491e446cd2
srinivas-devops7152 <idurgasrinivas@gmail.com>
2025-09-28 15:42:38 +0530

    robo shop tada

--- mongodb.sh
+++ mongodb.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FOLDER="/var/log/SHELL.SRINIVAS"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
